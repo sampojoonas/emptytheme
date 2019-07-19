d64bf399335e9fdb9507f901df7acea73a5c5e1c
Joonas Samposalo <joonas.samposalo@gmail.com>
2019-07-19 23:00:09 +0900

    comment out functions.php features

--- functions.php
+++ functions.php
@@ -6,10 +6,10 @@
 function empty_theme_scripts() {
 
     // スタイル
-    wp_enqueue_style('slider', get_template_directory_uri() . '/css/slider.css', false, '1.1', 'all');
+    wp_enqueue_style('main-style', get_template_directory_uri() . '/css/main.css', false, '1.1', 'all');
 
     // スクリプト
-    wp_enqueue_script('main', get_template_directory_uri() . '/css/slider.css', false, '1.1', true);
+    wp_enqueue_script('main-js', get_template_directory_uri() . '/css/main.js', false, '1.1', true);
 
 }
 add_action( 'wp_enqueue_scripts', 'empty_theme_scripts' );
@@ -17,12 +17,19 @@ add_action( 'wp_enqueue_scripts', 'empty_theme_scripts' );
 
 /* メニューの登録 */
 
+/*
 register_nav_menus(
     array(
     'header-menu' => __( 'ヘッダーメニュー' ),
     'footer-menu' => __( 'フッターメニュー' )
     )
 );
+*/
+
+/* アイキャッチ画像を有効にする */
+
+//add_theme_support( 'post-thumbnails' );
+//set_post_thumbnail_size( 800, 200, true ); // 幅、高さ、入らない部分と切り取るか？
 
 
 
